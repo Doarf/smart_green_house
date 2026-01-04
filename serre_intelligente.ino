bb65b30377be34e97626ce8c1abe83b5d8ec634c
Arthur Dogotaru <113799845+Doarf@users.noreply.github.com>
2026-01-04 20:25:55 +0100

    dht22 + ds20 + moisture + ultrason

--- serre_intelligente.ino
+++ serre_intelligente.ino
@@ -1,85 +1,83 @@
-
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <DHT.h>
-
-#define SCREEN_WIDTH 128 
-#define SCREEN_HEIGHT 64 
-#define BUTTON_PIN  18
-#define LED_PIN     5
-#define DHT22_PIN 23 
-int button_state = 0;
-Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1); 
-DHT dht22(DHT22_PIN, DHT22);
-
-String temperature;
-String humidity;
+#include "DHT.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+const int oneWireBus = 16;     
+OneWire oneWire(oneWireBus);
+DallasTemperature sensors(&oneWire);
+#define DHTPIN 4     
+#define DHTTYPE DHT22  
+#define AOUT_PIN 35 
+DHT dht(DHTPIN, DHTTYPE);
+const int trig_pin = 5;
+const int echo_pin = 18;
+#define SOUND_SPEED 340
+#define TRIG_PULSE_DURATION_US 10
+long ultrason_duration;
+float distance_cm;
 
 void setup() {
   Serial.begin(9600);
-  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    while (true);
-  }
-
-  delay(2000);        
-  oled.clearDisplay(); 
-  oled.setTextSize(3);     
-  oled.setTextColor(WHITE); 
-  oled.setCursor(0, 10);   
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  dht22.begin();             
-
-  temperature.reserve(10); 
-  humidity.reserve(10); 
+  dht.begin();
+  sensors.begin();
+  analogSetAttenuation(ADC_11db);
+  pinMode(trig_pin, OUTPUT); 
+  pinMode(echo_pin, INPUT); 
 }
 
+void ds20b18(){
+  sensors.requestTemperatures(); 
+  float temperatureC = sensors.getTempCByIndex(0);
+  float temperatureF = sensors.getTempFByIndex(0);
+  Serial.print(temperatureC);
+  Serial.println("ºC");
+  delay(1000);
+}
+
+void moisture(){
+    int value = analogRead(AOUT_PIN); 
+
+  Serial.print("Moisture value: ");
+  Serial.println(value);
+
+  delay(1000);
+}
+
+void ultrason(){
+  digitalWrite(trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(TRIG_PULSE_DURATION_US);
+  digitalWrite(trig_pin, LOW);
+  ultrason_duration = pulseIn(echo_pin, HIGH);
+  distance_cm = ultrason_duration * SOUND_SPEED/2 * 0.0001;
+  Serial.print("Distance (cm): ");
+  Serial.println(distance_cm);
+
+  delay(1000);
+}
+void dht22(){
+
+  delay(1000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Problème capteur DHT22"));
+    return;
+  }
+  float hif = dht.computeHeatIndex(f, h);
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.println(F("Humidité : "));
+  Serial.print(h);
+  Serial.println(F("%  Température: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+}
 void loop() {
-  float humi  = dht22.readHumidity();    
-  float tempC = dht22.readTemperature();
-  button_state = digitalRead(BUTTON_PIN);
-
-
-  if (button_state == LOW)       
-    digitalWrite(LED_PIN, HIGH); 
-  else                           
-    digitalWrite(LED_PIN, LOW);  
-
-  if (isnan(humi) || isnan(tempC)) {
-    temperature = "Failed";
-    humidity = "Failed";
-  } else {
-    temperature  = String(tempC, 1); 
-    temperature += char (247); 
-    temperature += "C";
-    humidity = String(humi, 1); 
-    humidity += "%";
-
-    Serial.print(tempC); 
-    Serial.print("°C | " ); 
-    Serial.print(humi);  
-    Serial.println("%");  
-  }
-
-  oledDisplayCenter(temperature, humidity); 
+  dht22();
+  ds20b18();
+  moisture();
+  ultrason();
 }
 
-void oledDisplayCenter(String temperature, String humidity) {
-  int16_t x1;
-  int16_t y1;
-  uint16_t width_T;
-  uint16_t height_T;
-  uint16_t width_H;
-  uint16_t height_H;
-
-  oled.getTextBounds(temperature, 0, 0, &x1, &y1, &width_T, &height_T);
-  oled.getTextBounds(temperature, 0, 0, &x1, &y1, &width_H, &height_H);
-  oled.clearDisplay(); 
-  oled.setCursor((SCREEN_WIDTH - width_T) / 2, SCREEN_HEIGHT/2 - height_T - 5);
-  oled.println(temperature); 
-  oled.setCursor((SCREEN_WIDTH - width_H) / 2, SCREEN_HEIGHT/2 + 5);
-  oled.println(humidity); 
-  oled.display();
-}
